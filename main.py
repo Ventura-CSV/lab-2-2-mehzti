85eaf51aba7ccfbd2b848e3a52e50f9810e2dcdf
Zyanya Garcia <zyanyagarcia8@gmail.com>
2023-08-25 16:50:44 -0700

    completed main.py

--- main.py
+++ main.py
@@ -4,17 +4,14 @@ def main():
     reg_rate = 18.25
     ov_rate = 27.78
 
-   ##################################################
-   # Code your program here
-   ##################################################
-    # overtime = workhours - reg_hours
-    # overtime_wage = overtime * ov_rate
-    # regular_wage = reg_hours * reg_rate
-    # total_wage = regular_wage + overtime_wage
+    overtime = (workhours - reg_hours)
+    overtime_wage = (overtime * ov_rate)
+    regular_wage = (reg_hours * reg_rate)
+    total_wage = (regular_wage + overtime_wage)
 
-    print(f"Regular hours: {reg_hours} Regular Charge: {regular_wage}")
-    print(f"Overtime hours: {overtime} Overtime Charge: {overtime_wage:.2f}")
-    print(f"Total wage : {total_wage:.2f}")
+    print(f"Regular Charge: {regular_wage}")
+    print(f"Overtime Charge: {overtime_wage:.2f}")
+    print(f"Total wage: {total_wage:.2f}")
 
    ##################################################
    # Do not delete the return statement
